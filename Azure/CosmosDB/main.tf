635ef0571b840227c3702d907404421133e88f2a
pchmielecki87 <pchmielecki87@gmail.com>
2021-11-05 08:24:43 +0100

    gitignore and cosmosdb

---
+++ Azure/CosmosDB/main.tf
@@ -0,0 +1,27 @@
+# Configure the Microsoft Azure Provider
+variable "subscription_id" {}
+
+variable "client_id" {}
+variable "client_secret" {}
+
+variable "tenant_id" {}
+
+provider "azurerm" {
+  subscription_id = "${var.subscription_id}"
+  client_id       = "${var.client_id}"
+  client_secret   = "${var.client_secret}"
+  tenant_id       = "${var.tenant_id}"
+}
+
+resource "azurerm_resource_group" "cosmos-rg" {
+  name     = "CosmosDemo"
+  location = "West US"
+}
+
+resource "azurerm_cosmosdb_account" "cosmosDB" {
+  name                = "cosmos-iac-demo"
+  location            = "${azurerm_resource_group.cosmos-rg.location}"
+  resource_group_name = "${azurerm_resource_group.cosmos-rg.name}"
+
+
+}
