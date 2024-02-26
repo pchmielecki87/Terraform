5e00c74907d8ef2384a1363c2633c3a738c1b95e
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-02-26 16:19:15 +0100

    add modules setup

--- Azure/Azure-Sandbox/3a-modules-and-pipeline/modules/terraform-rg/variables.tf
+++ Azure/Azure-Sandbox/3a-modules-and-pipeline/modules/terraform-rg/variables.tf
@@ -1,9 +1,9 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "The name of the resource group"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "The Azure region where resources will be provisioned"
 }
--- Azure/Azure-Sandbox/3a-modules-and-pipeline/variables.tf
+++ Azure/Azure-Sandbox/3a-modules-and-pipeline/variables.tf
@@ -1,5 +1,5 @@
 variable "resource_group_name" {
-  default     = ""
+  default = ""
   # variable declared here without provided value
 }
 
