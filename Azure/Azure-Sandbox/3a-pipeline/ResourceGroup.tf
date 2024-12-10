c1fe4364e4195a67615216d93ec0c5efb744a86e
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-12-10 07:44:42 +0100

    add az account show 2

--- Azure/Azure-Sandbox/3a-pipeline/ResourceGroup.tf
+++ Azure/Azure-Sandbox/3a-pipeline/ResourceGroup.tf
@@ -1,7 +1,5 @@
 provider "azurerm" {
   features {}
-  subscription_id = "8dac1cb1-7130-4137-a400-92c8b6c10bd9"
-  tenant_id = "63abcf25-b8df-4c76-8b31-5004c9ba37cd"
 }
 
 resource "azurerm_resource_group" "example" {
